45788faa442c99ab879c1c8e3e8e15d83e24e56a
Ilya <iilavr@mail.ru>
2022-08-10 17:45:53 +0300

    newbutton&text

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //MARK: - UILabel
+        label.text = "Hellow World!"
+        label.textColor = .blue
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.textAlignment = .center
+        label.numberOfLines = 5
+        
+        //MARK: - UIButton
+        button.backgroundColor = .green
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 10
+        button.setTitle("Touch Me", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        textField.textColor = .white
+        textField.textAlignment = .center
+        textField.placeholder = "type your text here..."
+        textField.backgroundColor = .systemBlue
     }
+    
+    @IBAction func buttonPresed(_ sender: UIButton) {
+        label.textColor = getRandomColor()
+        if textField.text != "" {
+            label.text = textField.text
+            textField.text = ""
+        }
+    }
+    
+    func getRandomColor() -> UIColor {
+         // Generate between 0 to 1
+         let red: CGFloat = CGFloat(drand48())
+         let green: CGFloat = CGFloat(drand48())
+         let blue: CGFloat = CGFloat(drand48())
 
-
+         return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
 
